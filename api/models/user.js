95176b8ba85e0186be1ace0f8b137e223dec6288
Karim MuHamad <karimmuhamadfci@gmail.com>
2021-02-19 02:36:31 +0200

    user model

---
+++ api/models/user.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+// const { ObjectId } = Schema.Types;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'subscriber'],
+        default: 'subscriber',
+    },
+    cart: {
+        type: Array,
+        default: [],
+    },
+    address: String,
+    // wishlist: [{
+    //     type: ObjectId,
+    //     ref: 'Product',
+    // }]
+}, {
+    timestamps: true,
+});
+
+userSchema.set('autoIndex', false);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
