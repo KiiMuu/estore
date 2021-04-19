8875be84f32c29fba48052d54b9fa454d60deb34
Karim MuuHammad <karimmuhamadfci@gmail.com>
2021-04-19 07:26:17 +0200

    make it better

--- app.js
+++ app.js
@@ -34,10 +34,12 @@ readdirSync('./routes').map(route => {
 });
 
 // * production
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
 
 // * app listening
 const port = process.env.PORT || 5000;
