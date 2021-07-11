b9657c4f6ea0c4cac2bf661ab1ed2fd260382340
Karim MuuHammad <karimmuhamadfci@gmail.com>
2021-07-11 05:18:49 +0200

    prepare to vercel

--- api/app.js
+++ api/app.js
@@ -34,12 +34,12 @@ readdirSync('./routes').map(route => {
 });
 
 // * production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'build')));
+//     app.get('/', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//     });
+// }
 
 // * app listening
 const port = process.env.PORT || 5000;
